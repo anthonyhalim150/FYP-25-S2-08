1e19704bd489068666b5f6180e696f387550ea76
anthonyhalim150 <anthonyhalim153@gmail.com>
2025-04-09 08:46:43 +0800

    Draft

--- fyp/wise-workout/wise-workout-backend/config/db.js
+++ fyp/wise-workout/wise-workout-backend/config/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Vvs319338',
-  database: 'FYP',
+  host: 'wise-workout.c9qquugywaez.ap-southeast-2.rds.amazonaws.com',
+  user: 'admin', 
+  password: 'adminAdmin1234#', 
+  database: 'FYP', 
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 100,
+  queueLimit: 5
 });
 
 module.exports = db;
