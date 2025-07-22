8d26023ead0fdccbb022fce86e9577acea4b39d4
anthonyhalim150 <anthonyhalim153@gmail.com>
2025-07-22 15:57:21 +0800

    add

--- fyp/wise-workout/wise-workout-app/lib/screens/questionnaires/questionnaire_screen_7.dart
+++ fyp/wise-workout/wise-workout-app/lib/screens/questionnaires/questionnaire_screen_7.dart
@@ -24,6 +24,7 @@ class _QuestionnaireScreen7State extends State<QuestionnaireScreen7> {
     'Wrist',
     'Knees',
     'Shoulder',
+    'No Injury'
     'Others (Type here)',
   ];
   final TextEditingController othersController = TextEditingController();
